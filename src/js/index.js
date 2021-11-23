fdd2500ae4e6f62536345e76795464ee5a825ed4
styura3000 <styura3000@gmail.com>
2021-11-23 10:33:41 +0300

    init

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+//import './tests';    //тесты работоспособности бабеля, лодаша, ленивой загрузки, еслинта
+import * as $ from 'jquery';
+import './jquery.nicescroll.min';
+import './slick.min';
+import './jq-start';
+import './script'
+import './jq-end'
+
+
+import '@/styles/styles.scss';
+
+
+
+
+
